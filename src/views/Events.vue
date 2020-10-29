9593562b2286be6afcb91e990c1e10603fc4bfbe
spindog <spindog.exe@gmail.com>
2020-10-29 21:18:38 +0800

    Modify pre-configs and add basic stuff

---
+++ src/views/Events.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Events View
+        <br />
+        <event />
+    </div>
+</template>
+
+<script>
+import Event from '@/components/Event.vue';
+
+export default {
+    name: 'Events',
+
+    components: {
+        Event,
+    },
+};
+</script>
