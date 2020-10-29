4b35502006070be432f213ddf248216b675eaa44
spindog <spindog.exe@gmail.com>
2020-10-29 22:55:05 +0800

    Install and configure npm packages

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+    router,
+    store,
+    render: h => h(App),
+}).$mount('#app');
