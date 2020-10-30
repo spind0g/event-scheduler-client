37d63738fa046b9d4aad3685f4eeed24a9b77443
spindog <spindog.exe@gmail.com>
2020-10-30 20:02:08 +0800

    Update axios defaults

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import store from './store';
 import axios from 'axios';
 import vuetify from './plugins/vuetify';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
+// axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
+axios.defaults.baseURL = 'https://aa-event-scheduler-api.herokuapp.com/api';
 
 Vue.config.productionTip = false;
 
